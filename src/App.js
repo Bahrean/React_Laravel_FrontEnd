f55817e1c62a5cc2d22b3984845ee01506fec2e0
Bahrean <bahreanplusplus@gmail.com>
2025-04-25 10:11:15 +0300

    upload project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import SuspectForm from './components/SuspectForm';
+
+function App() {
+  return (
+    <div>
+          <div className="min-h-screen bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
+      <SuspectForm />
+    </div>
+    </div>
+
+  );
+}
+
+export default App;      
